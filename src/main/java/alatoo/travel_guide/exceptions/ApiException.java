dd3ca9ae8a0a2e8540b0ee3a490819a4b8b29457
Iskha-69 <iskhak.koichubaev@alatoo.edu.kg>
2025-03-16 15:04:26 +0600

    created exception handler

---
+++ src/main/java/alatoo/travel_guide/exceptions/ApiException.java
@@ -0,0 +1,13 @@
+package alatoo.travel_guide.exceptions;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
+public class ApiException extends RuntimeException {
+    private final HttpStatusCode statusCode;
+    public ApiException(String message, HttpStatusCode code) {
+        super(message);
+        this.statusCode = code;
+    }
+}
